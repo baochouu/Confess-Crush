9b16b9f03d90ac5ff68f0b6e4b46a7756a3bf7f5
baochouu <90813784+baochouu@users.noreply.github.com>
2021-09-16 10:38:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "Hành tinh của Chouu",
-    introTitle: 'Ngọc',
+    introTitle: 'Quòa',
     introDesc: `hôm nay mệt mỏi rã rời
-    Chouu thèm trà sữa Ngọc mời được không`,
+    Chouu thèm trà sữa Quòa mời được không`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng Chouu đang thèm trà sữa =)',
-    desc: 'Phải chăng Ngọc sẽ mua trà sữa cho Chouu ',
+    desc: 'Phải chăng Quòa sẽ mua trà sữa cho Chouu ',
     btnYes: 'Mua cho Chou thật nhiều <33',
     btnNo: 'Không thèm mua cho Chouu :3',
-    question: 'Thế giới có nhiểu món nhưng Chou chỉ thích thịt crush và sữa<3',
+    question: 'Thế giới có nhiểu món nhưng Chou chỉ thích thịt crush và trà sữa<3',
     btnReply: 'Gửi cho Chou <3',
-    reply: 'Mua thì mua mà không mua thì mua <33333333',
-    mess: 'Mình biết mà <3. Yêu bạn nhiều nhiều 😘😘',
+    reply: 'mua thì mua không mua thì mua <33333333',
+    mess: 'Mình biết mà <3. Yêu Quòa nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, ship tà sữa cho mình nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/vskh.0712' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/2770417106408934/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
