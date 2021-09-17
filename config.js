006d6d369ad35b93913b46c98b972a6f7c655344
baochouu <90813784+baochouu@users.noreply.github.com>
2021-09-17 10:38:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Hành tinh của Chouu",
+    introTitle: 'Chợ đồ Hịu',
+    introDesc: `Hôm nay mệt mỏi rã rời. 
+    Mình thèm trà sữa mn mời dduocj không`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Phải chăng Chouu thèm trà sữa 🥰',
+    desc: 'Phải chăng mn mua trà swuax cho Chhouu ',
+    btnYes: 'Mua mua mua<33',
+    btnNo: 'Méo mua:3',
+    question: 'Điều quan trộng nói 2 lần: mua trà sữa cho Chou hong <3',
+    btnReply: 'Gửi cho Chou <3',
+    reply: 'Mua cho Chou <33333333',
+    mess: 'Mình biết mà 🥰. Yêu mn nhiều nhiều 😘😘',
+    messDesc: 'Lát đem qua cho Chou nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/2770417106408934' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
